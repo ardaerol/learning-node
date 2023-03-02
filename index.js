bc2c094a34897123c6421f6bf5a6d87c42e1910a
ardaerol <77547468+ardaerol@users.noreply.github.com>
2023-03-02 21:00:58 +0300

    02.03.2023

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+import {cikar, hello,text,topla} from "./myModul.js"
+
+console.log(topla(2,4))
+console.log(cikar(2,2))
+console.log(text)
+hello("arda")
---
+++ myModul.js
@@ -0,0 +1,13 @@
+
+const hello = name => {
+   // console.log("hello ",name)
+    console.log(`hello ${name}`)
+}
+
+const topla =  (a, b) =>  a + b
+const cikar =  (a, b) =>  a - b
+const text = 'text'
+
+export{hello,topla,cikar,text}
+
+
